b086e5bf22e4a55b9c1316147736d51a7ad334b3
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2022-04-23 17:46:13 -0300

    Show all warning creating a new exe

--- create_c_exe/create_c_exe.py
+++ create_c_exe/create_c_exe.py
@@ -12,8 +12,8 @@ def main(source_name):
     '''
     Compile the C source code
     '''
-    args_list = ['gcc', '-g', '-O2', '-std=gnu11', '-static', f'{source_name}.c', '-lm', '-o', f'{source_name}.exe']
-    result = (subprocess.run(args_list, stdout=subprocess.PIPE, encoding='utf-8').stdout).strip()
+    args_list = ['gcc', '-Wall','-g', '-O2', '-std=gnu11', '-static', f'{source_name}.c', '-lm', '-o', f'{source_name}.exe']
+    print(subprocess.run(args_list, stdout=subprocess.PIPE, encoding='utf-8').stdout)
 
 if __name__ == '__main__':
     # Create variable with arguments
