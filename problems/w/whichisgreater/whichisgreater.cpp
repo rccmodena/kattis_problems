fca1172876c44481da9492482d984d4f2973be2b
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2022-04-24 00:59:18 -0300

    CPP version of whichisgreater problem

---
+++ problems/w/whichisgreater/whichisgreater.cpp
@@ -0,0 +1,22 @@
+/*
+Author: Rudi César Comiotto Modena
+Email: rudi.modena@gmail.com
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a;
+    cin >> b;
+
+    if (a > b) {
+        cout << 1 << endl;
+    }
+    else {
+        cout << 0 << endl;
+    }
+}
