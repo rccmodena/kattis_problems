ae6ab7e348276069b6a0bab4d6eb7d60f622eb24
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2022-04-25 21:18:08 -0300

    Modified create_problem to get sample data files

--- create_problem/create_problem.py
+++ create_problem/create_problem.py
@@ -4,26 +4,35 @@ Author: Rudi César Comiotto Modena
 Email: rudi.modena@gmail.com
 """
 
-import sys
 import os
 import shutil
+import requests
+import zipfile
+import io
 
 
 def main(problem_name):
     '''
     Compile the C source code
     '''
+    tests_website = f"https://open.kattis.com/problems/{problem_name}/file/statement/samples.zip"
     problems_folder = r"D:\\my_stuff\\current\\sandbox\\kattis\\kattis\\problems\\"
 
     folder_name = problems_folder + problem_name[0] + r'\\' + problem_name + r'\\'
+    test_folder_name = folder_name + r'tests\\'
 
     if not os.path.exists(folder_name):
-        pass
         os.makedirs(folder_name)
         shutil.copyfile(problems_folder + 'base.c', folder_name + problem_name + '.c')
         shutil.copyfile(problems_folder + 'base.cpp', folder_name + problem_name + '.cpp')
         shutil.copyfile(problems_folder + 'base.md', folder_name + problem_name + '.md')
         shutil.copyfile(problems_folder + 'base.py', folder_name + problem_name + '.py')
+
+        os.makedirs(test_folder_name)
+        tests_request = requests.get(tests_website)
+        tests_zip = zipfile.ZipFile(io.BytesIO(tests_request.content))
+        tests_zip.extractall(test_folder_name)
+
     else:
         print(F"Problem <{problem_name}> already created!")
 
