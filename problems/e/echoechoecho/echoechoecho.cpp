232b12938db74a58647ed54a120a88008148ab17
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2022-04-29 13:39:12 -0300

    CPP echoechoecho problem

---
+++ problems/e/echoechoecho/echoechoecho.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Rudi César Comiotto Modena
+Email: rudi.modena@gmail.com
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string word;
+
+    cin >> word;
+
+    cout << word << " " << word << " " << word << endl;
+}
