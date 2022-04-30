3d3e7d200db5b17514047ab7f703002bb3f9bec6
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2022-04-30 18:50:58 -0300

    Addtwonumbers problem

---
+++ problems/a/addtwonumbers/addtwonumbers.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Rudi César Comiotto Modena
+Email: rudi.modena@gmail.com
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a;
+    cin >> b;
+
+    cout << a + b << endl;
+}
---
+++ problems/a/addtwonumbers/addtwonumbers.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+"""
+Author: Rudi César Comiotto Modena
+Email: rudi.modena@gmail.com
+"""
+
+def main():
+    a, b = [int(val) for val in input().split()]
+    print(a + b)
+
+
+if __name__ == '__main__':
+    main()
