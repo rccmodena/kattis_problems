373b649fe8c3338238085576e25e8e0f4843aa17
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2022-05-02 12:11:40 -0300

    R2 problem cpp

---
+++ problems/r/r2/r2.cpp
@@ -0,0 +1,18 @@
+/*
+Author: Rudi César Comiotto Modena
+Email: rudi.modena@gmail.com
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int r1, r2, s;
+
+    cin >> r1 >> s;
+
+    r2 = 2 * s - r1;
+
+    cout << r2 << endl;
+}
