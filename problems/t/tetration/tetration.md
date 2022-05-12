fce2283ebd8900b4f879ed487a616a228986e4c5
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2022-05-12 19:58:44 -0300

    Tetration problem python

---
+++ problems/t/tetration/tetration.md
@@ -0,0 +1,3 @@
+# TETRATION
+
+- [Problem Description](https://open.kattis.com/problems/tetration)
---
+++ problems/t/tetration/tetration.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+"""
+Author: Rudi César Comiotto Modena
+Email: rudi.modena@gmail.com
+"""
+
+def main():
+    n = float(input())
+
+    print(round(n ** (1/n), 6))
+
+
+if __name__ == '__main__':
+    main()
